cb79ca49a41e708824b33f2f35802ec219cb745d
yanismedouni <146112665+yanismedouni@users.noreply.github.com>
2025-03-13 20:23:10 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MortgageCalculator from './components/MortgageCalculator';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MortgageCalculator />
+    </div>
+  );
+}
+
+export default App;
